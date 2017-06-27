b9b258ef1dd8000ee152afec60ed88f715a9814d
dsfau <diego.soto.fau@gmail.com>
2017-06-27 08:55:46 +0200

    Update evelog_to_csv.py

--- evelog_to_csv.py
+++ evelog_to_csv.py
@@ -11,7 +11,7 @@ def flows_eve_json_to_csv(inputfile,outputfile)
 	log_final=open(csv, 'w')
 	with open(file) as log:
 	    p = log.readlines()
-	    log_final.write("date,src_ip,dst_ip,proto,src_port,dst_port,pkts,pktc,bytc,st,en,flgts,flgtc,flg,dur")
+	    log_final.write("date,src_ip,dst_ip,proto,src_port,dst_port,pkts,pktc,bytc,st,en,flgts,flgtc,flg,dur\n")
 	    for i in p:
 	        line=json.loads(i)
 		if line["event_type"] == "flow":
@@ -41,7 +41,7 @@ def flows_eve_json_to_csv(inputfile,outputfile)
 		                flgtc = 0
 		                flg = 0
 		            
-		        log_final.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}".format(date,src_ip,dst_ip,proto,src_port,
+		        log_final.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}\n".format(date,src_ip,dst_ip,proto,src_port,
 		                                                                               dst_port,pkts,pktc,bytc,st,en,flgts,
 		                                                                              flgtc,flg,dur))
 if __name__ == "__main__":
